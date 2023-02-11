49c1b1ca6c29c7d182a853a732006b3c43ebbae1
shubhamchandge <shubhamchandge@gmail.comm>
2023-02-11 10:34:49 +0530

    Add solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isClicked, SetisClicked] = useState("");
+
+  const handlechange = () => {
+    SetisClicked("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
+  console.log(SetisClicked);
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para">{isClicked}</p>
+      <br />
+      <button id="click" onClick={handlechange}> clickme</button>
     </div>
   );
 }
